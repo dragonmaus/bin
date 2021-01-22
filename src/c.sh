c6bd72d8a262b55831f8dba401992280b511c396
David Wildasin <dragonmaus@posteo.net>
2021-01-22 15:20:02 -0500

    Update clipboard commands:
    * Use xclip instead of xsel
    
    * Provide a consistent interface for choosing the selection

--- src/c.sh
+++ src/c.sh
@@ -1,2 +1,40 @@
 #!/bin/sh
-exec xsel -i "$@"
+
+set -e
+
+. echo.sh
+
+name=$(basename "$0" .sh)
+usage="Usage: $name [-h] [-cps]"
+help="$usage
+  -c   set the clipboard X selection (default)
+  -p   set the primary X selection
+  -s   set the secondary X selection
+
+  -h   display this help"
+
+selection=clipboard
+while getopts :chps opt
+do
+	case $opt in
+	(c)
+		selection=clipboard
+		;;
+	(h)
+		die 0 "$help"
+		;;
+	(p)
+		selection=primary
+		;;
+	(s)
+		selection=clipboard
+		;;
+	(*)
+		warn "$name: Unknown option '$OPTARG'"
+		die 100 "$usage"
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
+exec xclip -i -selection $selection
--- src/p.sh
+++ src/p.sh
@@ -1,2 +1,40 @@
 #!/bin/sh
-exec xsel -o "$@"
+
+set -e
+
+. echo.sh
+
+name=$(basename "$0" .sh)
+usage="Usage: $name [-h] [-cps]"
+help="$usage
+  -c   output the clipboard X selection (default)
+  -p   output the primary X selection
+  -s   output the secondary X selection
+
+  -h   display this help"
+
+selection=clipboard
+while getopts :chps opt
+do
+	case $opt in
+	(c)
+		selection=clipboard
+		;;
+	(h)
+		die 0 "$help"
+		;;
+	(p)
+		selection=primary
+		;;
+	(s)
+		selection=clipboard
+		;;
+	(*)
+		warn "$name: Unknown option '$OPTARG'"
+		die 100 "$usage"
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
+exec xclip -o -selection $selection
