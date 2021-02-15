ae452d658473bb15a8556f82af8466d067c19184
David Wildasin <dragonmaus@posteo.net>
2021-02-15 01:11:37 -0500

    sshmenu: Pass arguments to dmenu instead of ssh

--- src/sshmenu.sh
+++ src/sshmenu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-host=$(sed -n '/^# My local servers$/,/^# Public servers$/s/^Host \(.*\)$/\1/p' < ~/.ssh/config | sort -u | dmenu)
+host=$(sed -n '/^# My local servers$/,/^# Public servers$/s/^Host \(.*\)$/\1/p' < ~/.ssh/config | sort -u | dmenu "$@")
 [ -n "$host" ]
 
-exec st -e ssh "$host" "$@"
+exec st -e ssh "$host"
